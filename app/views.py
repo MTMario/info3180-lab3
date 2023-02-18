52eb07cd8ae0ad0412dbc60ab3a67f7680f801c2
Mario Thompson <mariothompson181998@gmail.com>
2023-02-18 02:21:39 -0500

    First form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -16,6 +18,19 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    """Render website's contact page."""
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+        flash('Your message was sent successfully')
+        return redirect(url_for('/'))
+    return render_template('contact.html', name=name,email=email,subject=subject,message=message)
 
 
 ###
